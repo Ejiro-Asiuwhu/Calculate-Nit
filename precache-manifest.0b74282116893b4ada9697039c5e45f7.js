961e0cc06c4f1ec03fac3500b6641c0629d40c23
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-02-03 16:07:19 +0100

    Updates

---
+++ precache-manifest.0b74282116893b4ada9697039c5e45f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c1b97fd48b7ab6dc35f22f909a6719",
+    "url": "/calculate-nit/index.html"
+  },
+  {
+    "revision": "81b5b656c2b88e3e0b5d",
+    "url": "/calculate-nit/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ee17f12e3ece4f24c824",
+    "url": "/calculate-nit/static/css/main.67597620.chunk.css"
+  },
+  {
+    "revision": "81b5b656c2b88e3e0b5d",
+    "url": "/calculate-nit/static/js/2.3256d1bf.chunk.js"
+  },
+  {
+    "revision": "af9c41958bfcab2a48903a96dd7576a8",
+    "url": "/calculate-nit/static/js/2.3256d1bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee17f12e3ece4f24c824",
+    "url": "/calculate-nit/static/js/main.4ca55e1f.chunk.js"
+  },
+  {
+    "revision": "058dda557b90842ea2a2",
+    "url": "/calculate-nit/static/js/runtime-main.6acf9e14.js"
+  }
+]);
